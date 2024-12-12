b2cb4c7c6d1d7c091df269af5afc60b982269b95
Mateus.gondev <mateus.gondev@gmail.com>
2024-12-12 20:02:56 -0300

    Atualização POST

--- src/main/java/com/mateus/example/projetospring/controllers/AlunoController.java
+++ src/main/java/com/mateus/example/projetospring/controllers/AlunoController.java
@@ -1,19 +1,35 @@
 package com.mateus.example.projetospring.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mateus.example.projetospring.Services.AlunoService;
+import com.mateus.example.projetospring.models.AlunoModel;
 import com.mateus.example.projetospring.repositories.AlunoRepository;
 
 @RestController
 @RequestMapping("/Aluno")
 public class AlunoController {
+
     // Ponto de injeção via construtor
     final AlunoRepository alunoRepository;
+    final AlunoService alunoService;
 
-    AlunoController(AlunoRepository alunoRepository) {
+    AlunoController(AlunoRepository alunoRepository, AlunoService alunoService) {
         this.alunoRepository = alunoRepository;
+        this.alunoService = alunoService;
+    }
+    // --
+
+    @PostMapping // Criar Entidade Aluno | Create (POST)
+    public ResponseEntity<AlunoModel> criarAluno(@RequestBody AlunoModel aluno) {
+        AlunoModel alunoSalvo = alunoRepository.save(aluno);
+        return ResponseEntity.created(URI.create("/alunos/" + alunoSalvo.getId())).body(alunoSalvo);
     }
 
 }
